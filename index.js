7532054dc84f706f560716c48b907b8b96d5bca1
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-15 13:51:59 +0200

    it works

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log('Hello, World!');
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import { URL } from "url";
+import dns from "dns";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const urlStore = {};
+let nextId = 1;
+
+app.post("/api/shorturl", (req, res) => {
+  const input = req.body.url;
+  if (!input) {
+    return res.status(400).json({ error: "Input is required" });
+  }
+  if (!/^https:?:\/\//i.test(input)) {
+    return res.status(400).json({ error: "Invalid URL format" });
+  }
+
+  let hostname = req.hostname;
+  try {
+    const parsed = new URL(input);
+    hostname = parsed.hostname;
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.status(400).json({ error: "Invalid URL" });
+    }
+
+    const id = nextId++;
+    urlStore[id] = input;
+    res.json({ original_url: input, short_url: id });
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const id = req.params.id;
+  const originalUrl = urlStore[id];
+  if (!originalUrl) {
+    return res.status(404).json({ error: "URL not found" });
+  }
+  res.redirect(originalUrl);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}\nhttp://localhost:${PORT}/`);
+});
